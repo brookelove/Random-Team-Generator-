75484686705416f026613f43a94112c0837acfe0
brookelove <brookelove@Brookes-iMac-2.local>
2022-10-31 16:49:45 -0700

    creating new file and short o hwo to do

---
+++ main.py
@@ -0,0 +1,7 @@
+import _random
+# this is a random generator for my students it will either in take the students name from a list and then generate the expected teams
+# asks for the number of people that needs to be in a group
+# takes that number and generates a random group
+
+
+group_num = input("How many groups do you need for your team?\n")
