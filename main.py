8e10e3491e72250d20fdad47f032d9fc68ca1d4d
brookelove <brookelove@Brookes-iMac-2.local>
2022-10-31 17:23:30 -0700

    adding in edge case of less names than groups

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 # this is a random generator for my students it will either in take the students name from a list and then generate the expected teams
 # asks for the number of people that needs to be in a group
 # takes that number and generates a random group
+student_names = []
 
 
 def random_groups(names, number_of_groups):
@@ -10,10 +11,10 @@ def random_groups(names, number_of_groups):
     random.shuffle(names)
     # create a new aray with teh list of lists of groups to create
     new_groups = []
-    # if (names <= number_of_groups):
-    #     # if the name is less than the number of groups exit the system
-    #     print("please make sure your names are moer than the students")
-    #     sys.exit()
+    if (len(names) <= int(number_of_groups)):
+        # if the name is less than the number of groups exit the system
+        print("Please make sure your names are more than the groups.")
+        sys.exit()
     # the number in the for loop will be looped as many times as the group input
     for index in range(number_of_groups):
         # the index is the current group and will loop at the range in the number of group
