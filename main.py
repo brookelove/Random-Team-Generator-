4d05bb29cf56d19dc4e4c1379faf47dec290e4b7
brookelove <brookelove@Brookes-iMac-2.local>
2022-10-31 17:09:37 -0700

    correct errorrs and troubleshooting

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import sys
 # this is a random generator for my students it will either in take the students name from a list and then generate the expected teams
 # asks for the number of people that needs to be in a group
 # takes that number and generates a random group
-student_names = []
 
 
 def random_groups(names, number_of_groups):
@@ -11,11 +10,11 @@ def random_groups(names, number_of_groups):
     random.shuffle(names)
     # create a new aray with teh list of lists of groups to create
     new_groups = []
-    if (names <= number_of_groups):
-        # if the name is less than the number of groups exit the system
-        print("please make sure your names are moer than the students")
-        sys.exit()
-        # the number in the for loop will be looped as many times as the group input
+    # if (names <= number_of_groups):
+    #     # if the name is less than the number of groups exit the system
+    #     print("please make sure your names are moer than the students")
+    #     sys.exit()
+    # the number in the for loop will be looped as many times as the group input
     for index in range(number_of_groups):
         # the index is the current group and will loop at the range in the number of group
         group = names[index::number_of_groups]
@@ -28,4 +27,4 @@ def random_groups(names, number_of_groups):
 
 group_num = input("How many groups do you need for your team?\n")
 
-random_groups(student_names, group_num)
+random_groups(student_names, int(group_num))
