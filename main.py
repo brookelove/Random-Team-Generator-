22001240182532cf11fbf59c7b1e091519b1b44e
brookelove <brookelove@Brookes-iMac-2.local>
2022-10-31 17:04:24 -0700

    finishing groups created and and names

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
-import _random
+import random
+import sys
 # this is a random generator for my students it will either in take the students name from a list and then generate the expected teams
 # asks for the number of people that needs to be in a group
 # takes that number and generates a random group
+student_names = []
+
+
+def random_groups(names, number_of_groups):
+    # need to shuffle the names
+    random.shuffle(names)
+    # create a new aray with teh list of lists of groups to create
+    new_groups = []
+    if (names <= number_of_groups):
+        # if the name is less than the number of groups exit the system
+        print("please make sure your names are moer than the students")
+        sys.exit()
+        # the number in the for loop will be looped as many times as the group input
+    for index in range(number_of_groups):
+        # the index is the current group and will loop at the range in the number of group
+        group = names[index::number_of_groups]
+        # will append the current group to the new groups made
+        new_groups.append(group)
+    # run through groups and create a new group
+    for index, group in enumerate(new_groups):
+        print(f"Group {index + 1} : {' , '.join(group)}\n")
 
 
 group_num = input("How many groups do you need for your team?\n")
+
+random_groups(student_names, group_num)
